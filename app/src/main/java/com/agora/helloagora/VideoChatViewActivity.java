d1822efc52691eac17bd022add628eb77c698f12
Shaocheng Yang <shaocheng.yang1995@gmail.com>
2019-11-11 23:32:10 -0800

    Restructer the code and add dimens for different screen density

--- app/src/main/java/com/agora/helloagora/VideoChatViewActivity.java
+++ app/src/main/java/com/agora/helloagora/VideoChatViewActivity.java
@@ -16,14 +16,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class VideoChatViewActivity extends AppCompatActivity {
+    private static final String TAG = VideoChatViewActivity.class.getName();
+
     private static final int PERMISSION_REQ_ID = 22;
     RtcEngine mRtcEngine;
     private RelativeLayout mRemoteContainer;
     private SurfaceView mRemoteView;
+    private FrameLayout mLocalContainer;
+    private SurfaceView mLocalView;
+    private ImageView mCallBtn, mMuteBtn, mSwitchCameraBtn;
 
     // Ask for Android device permissions at runtime.
     private static final String[] REQUESTED_PERMISSIONS = {
@@ -31,58 +38,6 @@ public class VideoChatViewActivity extends AppCompatActivity {
             Manifest.permission.CAMERA,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video_chat_view);
-
-        if (checkSelfPermission(REQUESTED_PERMISSIONS[0], PERMISSION_REQ_ID) &&
-                checkSelfPermission(REQUESTED_PERMISSIONS[1], PERMISSION_REQ_ID) &&
-                checkSelfPermission(REQUESTED_PERMISSIONS[2], PERMISSION_REQ_ID)) {
-            initEngineAndJoinChannel();
-        }
-
-    }
-
-    private void setupLocalVideo() {
-
-        // Enable the video module.
-        mRtcEngine.enableVideo();
-
-        // Create a SurfaceView object.
-        FrameLayout mLocalContainer = new FrameLayout(this);
-        SurfaceView mLocalView;
-
-        mLocalView = RtcEngine.CreateRendererView(getBaseContext());
-        mLocalView.setZOrderMediaOverlay(true);
-        mLocalContainer.addView(mLocalView);
-        // Set the local video view.
-        VideoCanvas localVideoCanvas = new VideoCanvas(mLocalView, VideoCanvas.RENDER_MODE_HIDDEN, 0);
-        mRtcEngine.setupLocalVideo(localVideoCanvas);
-    }
-
-    private void joinChannel() {
-        // Join a channel with a token.
-        mRtcEngine.joinChannel(getString(R.string.temp_token), "Andrew", "Extra Optional Data", 0);
-    }
-
-    private void initEngineAndJoinChannel() {
-        initializeEngine();
-        setupLocalVideo();
-        joinChannel();
-    }
-
-    private boolean checkSelfPermission(String permission, int requestCode) {
-        if (ContextCompat.checkSelfPermission(this, permission) !=
-                PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, REQUESTED_PERMISSIONS, requestCode);
-            return false;
-        }
-
-        return true;
-    }
 
     private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
         @Override
@@ -125,6 +80,84 @@ public class VideoChatViewActivity extends AppCompatActivity {
         }
     };
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chat_view);
+        initUI();
+
+        if (checkSelfPermission(REQUESTED_PERMISSIONS[0], PERMISSION_REQ_ID) &&
+                checkSelfPermission(REQUESTED_PERMISSIONS[1], PERMISSION_REQ_ID) &&
+                checkSelfPermission(REQUESTED_PERMISSIONS[2], PERMISSION_REQ_ID)) {
+            initEngineAndJoinChannel();
+        }
+
+    }
+
+    private void initUI() {
+        mLocalContainer = findViewById(R.id.local_video_view_container);
+        mRemoteContainer = findViewById(R.id.remote_video_view_container);
+
+        mCallBtn = findViewById(R.id.btn_call);
+        mMuteBtn = findViewById(R.id.btn_mute);
+        mSwitchCameraBtn = findViewById(R.id.btn_switch_camera);
+
+    }
+
+    private void initEngineAndJoinChannel() {
+        initializeEngine();
+        setupLocalVideo();
+        joinChannel();
+    }
+
+    private void initializeEngine() {
+        try {
+            mRtcEngine = RtcEngine.create(getBaseContext(), getString(R.string.agora_app_id), mRtcEventHandler);
+        } catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+            throw new RuntimeException("NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(e));
+        }
+    }
+
+    private void setupLocalVideo() {
+
+        // Enable the video module.
+        mRtcEngine.enableVideo();
+
+        mRtcEngine.setVideoEncoderConfiguration(new VideoEncoderConfiguration(
+                VideoEncoderConfiguration.VD_640x360,
+                VideoEncoderConfiguration.FRAME_RATE.FRAME_RATE_FPS_15,
+                VideoEncoderConfiguration.STANDARD_BITRATE,
+                VideoEncoderConfiguration.ORIENTATION_MODE.ORIENTATION_MODE_FIXED_PORTRAIT));
+
+        mLocalView = RtcEngine.CreateRendererView(getBaseContext());
+        mLocalView.setZOrderMediaOverlay(true);
+        mLocalContainer.addView(mLocalView);
+        // Set the local video view.
+        VideoCanvas localVideoCanvas = new VideoCanvas(mLocalView, VideoCanvas.RENDER_MODE_HIDDEN, 0);
+        mRtcEngine.setupLocalVideo(localVideoCanvas);
+    }
+
+    private void joinChannel() {
+        // Join a channel with a token.
+        mRtcEngine.joinChannel(getString(R.string.temp_token), "Andrew", "Extra Optional Data", 0);
+    }
+
+
+
+    private boolean checkSelfPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(this, permission) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, REQUESTED_PERMISSIONS, requestCode);
+            return false;
+        }
+
+        return true;
+    }
+
+
+
     private void onRemoteUserLeft() {
         removeRemoteVideo();
     }
@@ -138,24 +171,15 @@ public class VideoChatViewActivity extends AppCompatActivity {
 
     private void setupRemoteVideo(int uid) {
 
-        // Create a SurfaceView object.
-        mRemoteContainer = new RelativeLayout(this);
-
         mRemoteView = RtcEngine.CreateRendererView(getBaseContext());
         mRemoteContainer.addView(mRemoteView);
         // Set the remote video view.
         mRtcEngine.setupRemoteVideo(new VideoCanvas(mRemoteView, VideoCanvas.RENDER_MODE_HIDDEN, uid));
+        mRemoteView.setTag(uid);
 
     }
 
-    private void initializeEngine() {
-        try {
-            mRtcEngine = RtcEngine.create(getBaseContext(), getString(R.string.agora_app_id), mRtcEventHandler);
-        } catch (Exception e) {
-            Log.e("initializeEngine", Log.getStackTraceString(e));
-            throw new RuntimeException("NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(e));
-        }
-    }
+
 
     @Override
     protected void onDestroy() {
